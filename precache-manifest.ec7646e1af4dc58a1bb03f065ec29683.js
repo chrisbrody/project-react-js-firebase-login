f8270507ba7b358a1ec2b1e1690feb54ebb5c68c
Chris Brody <chrisbrody@codifyacademy.com>
2019-12-26 11:51:49 -0500

    Updates

---
+++ precache-manifest.ec7646e1af4dc58a1bb03f065ec29683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62675cc0d50651c2e0fa5a6d816f601",
+    "url": "/project-react-js-firebase-login/index.html"
+  },
+  {
+    "revision": "b8a4ee517c8da163710e",
+    "url": "/project-react-js-firebase-login/static/css/main.ac24c0fa.chunk.css"
+  },
+  {
+    "revision": "945388e82f2a0140d8f9",
+    "url": "/project-react-js-firebase-login/static/js/2.2d89b294.chunk.js"
+  },
+  {
+    "revision": "cfae6de4e4fba91c363679a64a6e3590",
+    "url": "/project-react-js-firebase-login/static/js/2.2d89b294.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8a4ee517c8da163710e",
+    "url": "/project-react-js-firebase-login/static/js/main.d924f3b2.chunk.js"
+  },
+  {
+    "revision": "538ea80a82fa3368f1fe",
+    "url": "/project-react-js-firebase-login/static/js/runtime-main.46781070.js"
+  }
+]);
